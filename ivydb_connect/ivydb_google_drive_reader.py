8ef8df7573b88556fe3253d39170903bb44d13e5
stirlitzzz <ymulman@yahoo.com>
2024-09-01 22:32:58 -0400

    changed to read all floats from files than convert to int so it can handle nan

--- ivydb_connect/ivydb_google_drive_reader.py
+++ ivydb_connect/ivydb_google_drive_reader.py
@@ -6,6 +6,7 @@ from googleapiclient.http import MediaIoBaseDownload
 import pandas as pd
 import numpy as np
 from ivydb_utils import IvyDBUtils
+import tqdm
 
 class IvyDBGoogleDriveReader:
     def __init__(self, credentials_file: str, folder_id: str, definitions_file: str):
@@ -49,18 +50,24 @@ class IvyDBGoogleDriveReader:
         file_content = self.download_and_extract(file_id)
         
         dtype_dict = self.ivy_db.get_dtype_dict(table_name)
+        float_dtype_dict = {col: 'float64' if dtype.startswith('int') else dtype for col, dtype in dtype_dict.items()}
+
         date_columns = [col['name'] for col in self.ivy_db.get_table_structure(table_name) if col['type'] == 'date']
         
         df = pd.read_csv(
             file_content,
             delimiter='\t',
             names=self.ivy_db.get_column_names(table_name),
-            dtype=dtype_dict,
-            parse_dates=date_columns
+            dtype=float_dtype_dict,
+            parse_dates=date_columns,
+            na_values=['-99.99', 'nan', 'NaN', '']  # Add any other strings that represent NaN in your data
         )
+
+        for col, dtype in dtype_dict.items():
+            if dtype.startswith('int'):
+                df[col] = pd.to_numeric(df[col], errors='coerce').astype('Int64')
         
         # Handle missing values (assuming -99.99 is used for missing values)
-        df = df.replace(-99.99, np.nan)
         
         return df
 
@@ -74,8 +81,11 @@ if __name__ == "__main__":
     
     try:
         df = reader.read_file('IVYSECPRD_199601.zip', 'Security_Price')
+        df2= reader.read_file('IVYOPPRCD_199601.zip', 'Option_Price')
         print(df.head())
         print(df.dtypes)
+        print(df2.head())
+        print(df2.dtypes)
     except FileNotFoundError as e:
         print(f"Error: {e}")
     except Exception as e:
